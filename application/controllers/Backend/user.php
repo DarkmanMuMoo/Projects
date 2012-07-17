ad4de15f7df8866624da240a5d388d1cfc7818ab
MuMoo <darkmanmumoonaja@gmail.com>
2012-07-18 01:26:59 +0700

    แก้

--- application/controllers/Backend/user.php
+++ application/controllers/Backend/user.php
@@ -12,13 +12,56 @@
  */
 class User extends CI_Controller {
     //put your code here
-    
+     public function __construct() {
+        parent::__construct();
+        
+        $this->load->model('dao/empdao');
+        //$this->load->model('obj/custormer');  loaded in autoload
+         $this->load->library('form_validation');
+    }
+ 
      public function performlogin(){
-         
-         
-         
-         
+              $password=$this->input->post('password'); 
+  
+   
+       $this->form_validation->set_rules('name', 'Name', "callback_user_check[$password]");
+         if ($this->form_validation->run() == FALSE) {
+              
+             //redirect('false');
+         $this->load->view(lang('baklogin'));
+             //$this->load->view('dsf');
+        }else{
+            
+            $_SESSION['emp']=$this->empdao->findbyid($this->input->post('name') );
+            
+             $this->load->view(lang('bakhome'));
+        }
      }
+           public function user_check($name,$password) {
+               
+               
+               
+               $emp=null;
+
+         $emp = $this->empdao->findbyid($name);
+
+        if ( $emp==null) {
+
+            $this->form_validation->set_message('user_check', 'ไม่มีพนักงาน นี้ในระบบ');
+            return FALSE;
+             
+        } else if($emp->getPassword()!= $password ){
+        
+             $this->form_validation->set_message('user_check', 'password ไม่ถุกต้อง');
+            return FALSE;
+          
+            }else{
+   
+                return TRUE; 
+               
+            }
+           }
+     
 }
 
 ?>
