00c9c0299d37d9c9df6ed77f6a8d53ad157eb78b
MungMink <petulant_pm@hotmail.com>
2012-09-17 17:11:34 +0700

    sหิว

--- application/views/OnlinePrinting/Orders/OrderSumary.php
+++ application/views/OnlinePrinting/Orders/OrderSumary.php
@@ -1,6 +1,17 @@
   <? $this->load->view(lang('header')) ?>
-            <p>
-                Summary 
-            </p>
+  <style>
+   hr{color: orangeRed;
+       background-color: orange;
+       height: 1px;
+    }
+  </style>
+  
+<p style ="margin-bottom: 10px;">
+    <h1><b>Order Summary</b></h1>
+    <h4></h4>
+</p>
+<hr></hr>
+
+
             
   <? $this->load->view(lang('footer')) ?>
