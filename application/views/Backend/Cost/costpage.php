2b2eba90fcc4a8fc77d99cb2dc0cde4bf29ad3ee
MungMink <petulant_pm@hotmail.com>
2012-09-19 16:12:52 +0700

    ไรไม่รุ้

--- application/views/Backend/Cost/costpage.php
+++ application/views/Backend/Cost/costpage.php
@@ -1,17 +1,81 @@
 <? $this->load->view(lang('bakheader'));?>
 
+<style>
+#table{
+	border-color:#CCC;
+	border:thin;
+	align}
+</style>
+
 <div class="container" >
+
+</div>
     <div id='mainmenu' >
         <ul>
             <a class="btn" href="<? echo site_url('Backend/bakCost/template') ?>"><span>Template</span> </a></ul>
           
           </ul> 
-        
-    </div>
-    
-    
-    
-</div>
+         <input type="radio" id="paper" name="upd" value="upd"  /><label for="upd">กระดาษ</label>
+ 
+<input type="radio" id="send" name="send" value="send"  /><label for="send">การจัดส่ง</label>
+ 
+ <input type="radio" id="option" name="option" value="option"  /><label for="option">ตัวเลือกพิเศษ</label>
+ 
 
+
+<div>
+<form action="" method="post">
+<table width="369" border="1" bordercolor="#CCCCCC">
+  <tr>
+    <td width="145">ชนิดกระดาษ</td>
+    <td width="57">แกรม</td>
+    <td width="145">ราคา</td>
+  </tr>
+  <tr>
+    <td>อาร์ต</td>
+    <td>160</td>
+    <td><input type="text"/></td>
+  </tr>
+  </tr>
+
+</table>
+<br /><input type="button" value="แก้ไข"/>
+</form>
+
+
+
+<form action="" method="post">
+<table  table width="269" border="1" bordercolor="#CCCCCC" >
+  <tr>
+    <td width="109">วิธีการส่ง</td>
+    <td width="144">ราคา</td>
+  </tr>
+  <tr>
+    <td>ไปรษณีย์</td>
+    <td><input type="text"/></td>
+  </tr>
+  
+</table>
+<br /><input type="button" value="แก้ไข"/>
+</form>
+
+
+<form action="" method="post">
+<table  table width="332" border="1" bordercolor="#CCCCCC" >
+  <tr>
+    <td width="132">ตัวเลือกพิเศษ</td>
+    <td width="184">ราคา</td>
+  </tr>
+  <tr>
+    <td>ไปรษณีย์</td>
+    <td><input type="text"/></td>
+  </tr>
+  
+</table>
+<br /><input type="button" value="แก้ไข"/>
+</form>
+    </div>
+
+</div>
 
 <? $this->load->view(lang('bakfooter'));?>
--- application/views/Backend/Cost/templatedetail.php
+++ application/views/Backend/Cost/templatedetail.php
@@ -2,9 +2,51 @@
 
 <div class="container" >
  
-    
-    
-    
+    <form action="" method="get"><table width="457" border="1" bordercolor="#CCCCCC">
+  <tr>
+    <td width="124">เทมเพลต</td>
+    <td width="80">ขนาด</td>
+    <td width="239">url</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>&nbsp;</td>
+    <td><input type="file" /></td>
+  </tr>
+
+</table>
+<br /><input type="button" value="แก้ไข"/>
+</form>
+
+<form action="" method="get">
+<table width="230" border="0" >
+  <tr>
+    <td width="174">X</td>
+    <td width="46"><select name="">
+    <option></option></select></td>
+  </tr>
+  <tr>
+    <td>Y</td>
+    <td><select name="">
+    <option></option></select></td>
+  </tr>
+  <tr>
+    <td>Z</td>
+    <td><select name="">
+    <option></option></select></td>
+  </tr>
+  <tr>
+    <td>platesize</td>
+    <td><select name="">
+    <option>L</option>
+    <option>S</option></select></td>
+  </tr>
+
+</table>
+<br /><input type="button" value="แก้ไข"/>
+</form>
+
+
 </div>
 
 
