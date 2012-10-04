da948d9dd8479a40d3bd273d703b2c3b48fb4656
MungMink <petulant_pm@hotmail.com>
2012-10-04 19:40:47 +0700

    งง

--- application/logs/log-2012-10-04.php
+++ application/logs/log-2012-10-04.php
@@ -41,3 +41,38 @@ ERROR - 2012-10-04 08:40:23 --> 404 Page Not Found --> userprofile/favicon.ico.b
 ERROR - 2012-10-04 08:42:26 --> 404 Page Not Found --> userprofile/favicon.ico.bmp
 ERROR - 2012-10-04 08:44:49 --> 404 Page Not Found --> userprofile/favicon.ico.bmp
 ERROR - 2012-10-04 08:45:03 --> 404 Page Not Found --> userprofile/favicon.ico.bmp
+ERROR - 2012-10-04 09:03:53 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:03:54 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:03:56 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:04:05 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:04:05 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:04:19 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:05:09 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:05:10 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-04 09:12:37 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:12:37 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:12:37 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:12:37 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:12:37 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:12:37 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:12:37 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:12:37 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:13:52 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:17:05 --> 404 Page Not Found --> asset
+ERROR - 2012-10-04 09:17:17 --> 404 Page Not Found --> asset
+ERROR - 2012-10-04 09:17:31 --> 404 Page Not Found --> asset
+ERROR - 2012-10-04 09:17:47 --> 404 Page Not Found --> asset
+ERROR - 2012-10-04 09:19:11 --> Query error: Column 'active' cannot be null
+ERROR - 2012-10-04 09:27:28 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:27:28 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:27:28 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:27:28 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:27:28 --> Severity: Notice  --> Undefined variable: chkemp C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
+ERROR - 2012-10-04 09:27:28 --> Severity: Warning  --> in_array() expects parameter 2 to be array, null given C:\wamp\www\Projects\application\views\Backend\Work\workdetail.php 53
