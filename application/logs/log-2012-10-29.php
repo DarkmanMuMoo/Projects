72d56a46f2a7c6a1564721fd7ff88a4c816b4eca
MungMink <petulant_pm@hotmail.com>
2012-10-29 14:52:05 +0700

    qwer

---
+++ application/logs/log-2012-10-29.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-10-29 07:29:17 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-29 07:29:17 --> 404 Page Not Found --> product/favicon.ico.bmp
+ERROR - 2012-10-29 07:29:19 --> 404 Page Not Found --> orders/favicon.ico.bmp
+ERROR - 2012-10-29 07:29:23 --> 404 Page Not Found --> orders/favicon.ico.bmp
+ERROR - 2012-10-29 07:30:35 --> Severity: Warning  --> file_get_contents(http://localhost/Projects/uploads/ปิยกานต์_2012-10-29_07-30-30.ai) [<a href='function.file-get-contents'>function.file-get-contents</a>]: failed to open stream: HTTP request failed! HTTP/1.0 400 Bad Request
+ C:\wamp\www\Projects\application\controllers\orders.php 34
+ERROR - 2012-10-29 07:30:35 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at C:\wamp\www\Projects\system\core\Exceptions.php:185) C:\wamp\www\Projects\system\core\Output.php 391
+ERROR - 2012-10-29 07:30:51 --> Severity: Warning  --> file_get_contents(http://localhost/Projects/uploads/ปิยกานต์_2012-10-29_07-30-30.ai) [<a href='function.file-get-contents'>function.file-get-contents</a>]: failed to open stream: HTTP request failed! HTTP/1.0 400 Bad Request
+ C:\wamp\www\Projects\application\controllers\orders.php 34
+ERROR - 2012-10-29 07:30:51 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at C:\wamp\www\Projects\system\core\Exceptions.php:185) C:\wamp\www\Projects\system\core\Output.php 391
